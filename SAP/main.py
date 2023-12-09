341c6e88204d0a5f07f4e55e86f6096860ab9705
Ionescu Mihai-Cosmin <ionescucosmin2003@gmail.com>
2023-12-10 00:16:56 +0200

    Implemented plotting functionality

--- SAP/main.py
+++ SAP/main.py
@@ -1,21 +1,11 @@
 from sys import exit
+
+import prophet
 from prophet import Prophet
 import pandas as pd
 import numpy as np
 import matplotlib as mp
 import logging
-
-
-def prediction(data):
-    data.rename(columns={'Date': 'ds', 'Sales': 'y'}, inplace=True)
-    data['ds'] = pd.to_datetime(data['ds'])
-    m = Prophet()
-    m.fit(data)
-
-    future = m.make_future_dataframe(periods=36, freq='MS')
-    forecast = m.predict(future)
-    return forecast
-
 
 def main():
     # read the Excel file
@@ -28,20 +18,39 @@ def main():
     df = pd.read_excel("sales_and_eodStocks.xlsx", sheet_name="Sheet1")
     df = df.reset_index()
 
+    models = {}
     forecasts = {}
     row_list = []
+
     for index, row in df.iterrows():
         if len(row_list) == 0:
             row_list.append(row)
             id = str(row["Product_ID"])
+
         elif str(row["Product_ID"]) == id:
             row_list.append(row)
+
         else:
             data = pd.DataFrame(row_list)
             row_list.clear()
             row_list.append(row)
+
             if data.shape[0] > 1:
-                forecasts[id] = prediction(data)
+                data.rename(columns={'Date': 'ds', 'Sales': 'y'}, inplace=True)
+                data['ds'] = pd.to_datetime(data['ds'])
+
+                m = Prophet(yearly_seasonality=True, changepoint_range=0.8, changepoint_prior_scale=0.05)
+                m.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+                m.fit(data)
+                future = m.make_future_dataframe(periods=24, freq='MS')
+
+                models[id] = m
+                forecasts[id] = m.predict(future)
+
+    fig = models.get('15036').plot(forecasts.get('15036'), uncertainty=False, plot_cap=False, include_legend=True)
+    fig.show()
+    fig = models.get('15036').plot_components(forecasts.get('15036'))
+    fig.show()
 
 
 # Fisierul asta trebuie rulat din terminal pentru a rula programul
