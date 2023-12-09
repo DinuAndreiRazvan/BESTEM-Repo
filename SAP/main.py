fbc3c0dd4749bca14649a8deebe80e0ef49128ee
Dinu Andrei-Razvan <anndrei014@gmail.com>
2023-12-09 13:27:09 +0200

    Reading

---
+++ SAP/main.py
@@ -0,0 +1,44 @@
+import datetime
+import pandas as pd
+from dataclasses import dataclass
+
+
+@dataclass
+class Stock:
+    date: datetime.date
+    product_ID: str
+    sales: int
+    endOfDayStock: int
+
+@dataclass
+class Transaction:
+    invoice : int
+    product_ID : str
+    description : str
+    quantity : int
+    date : datetime.datetime
+    price : float
+    customer_ID : str
+    country : str
+
+
+
+# read the Excel file
+df = pd.read_excel('sales_and_eodStocks.xlsx', sheet_name='Sheet1')
+
+# create a list of student objects
+stocks = []
+for _, row in df.iterrows():
+    stocks.append(Stock(row['Date'], row['Product_ID'], row['Sales'], row['EndOfDayStock']))
+
+
+# read the Excel file
+df = pd.read_excel('transactions.xlsx', sheet_name='Sheet1')
+
+# create a list of student objects
+transactions = []
+for _, row in df.iterrows():
+    transactions.append(Transaction(row['Invoice'], row['Product_ID'], row['Description'], 
+                        row['Quantity'], row['Date'], row['Price'], row['Customer ID'], row['Country']))
+
+print(transactions)
