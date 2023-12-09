7505c949bbb4b8ecfd7cff29b3305d9e57ff8303
Ionescu Mihai-Cosmin <ionescucosmin2003@gmail.com>
2023-12-09 14:16:56 +0200

    Cleaned up some files

--- SAP/main.py
+++ SAP/main.py
@@ -10,35 +10,33 @@ class Stock:
     sales: int
     endOfDayStock: int
 
+
 @dataclass
 class Transaction:
-    invoice : int
-    product_ID : str
-    description : str
-    quantity : int
-    date : datetime.datetime
-    price : float
-    customer_ID : str
-    country : str
+    invoice: int
+    product_ID: str
+    description: str
+    quantity: int
+    date: datetime.datetime
+    price: float
+    customer_ID: str
+    country: str
 
 
-
-# read the Excel file
+# read the Stocks Excel file
 df = pd.read_excel('sales_and_eodStocks.xlsx', sheet_name='Sheet1')
 
-# create a list of student objects
+# create a list of Stock objects
 stocks = []
 for _, row in df.iterrows():
     stocks.append(Stock(row['Date'], row['Product_ID'], row['Sales'], row['EndOfDayStock']))
 
-
-# read the Excel file
+# read the Transactions Excel file
 df = pd.read_excel('transactions.xlsx', sheet_name='Sheet1')
 
-# create a list of student objects
+# create a list of Transaction objects
 transactions = []
 for _, row in df.iterrows():
-    transactions.append(Transaction(row['Invoice'], row['Product_ID'], row['Description'], 
-                        row['Quantity'], row['Date'], row['Price'], row['Customer ID'], row['Country']))
-
-print(transactions)
+    transactions.append(Transaction(row['Invoice'], row['Product_ID'], row['Description'],
+                                    row['Quantity'], row['Date'], row['Price'],
+                                    row['Customer ID'], row['Country']))
