79294492352efb026490f179812f6b6799632a02
DariusIuga <dariusiuga2@gmail.com>
2023-12-10 04:28:47 +0200

    Added graphs to site

--- SAP/frontend/src/script.js
+++ SAP/frontend/src/script.js
@@ -1,46 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Sample data
-    const salesData = [50, 120, 80, 200, 100, 150];
-    const categoryData = [30, 20, 25, 15, 10];
+"use strict";
 
-    // Sales chart
-    const salesCtx = document.getElementById("salesChart").getContext("2d");
-    new Chart(salesCtx, {
-        type: "bar",
-        data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-            datasets: [
-                {
-                    label: "Sales",
-                    data: salesData,
-                    backgroundColor: "rgba(75, 192, 192, 0.2)",
-                    borderColor: "rgba(75, 192, 192, 1)",
-                    borderWidth: 1,
-                },
-            ],
-        },
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Sample data
+//     const salesData = [50, 120, 80, 200, 100, 150];
+//     const categoryData = [30, 20, 25, 15, 10];
 
-    // Category chart
-    const categoryCtx = document
-        .getElementById("categoryChart")
-        .getContext("2d");
-    new Chart(categoryCtx, {
-        type: "doughnut",
-        data: {
-            labels: ["Electronics", "Clothing", "Books", "Home", "Toys"],
-            datasets: [
-                {
-                    data: categoryData,
-                    backgroundColor: [
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56",
-                        "#4CAF50",
-                        "#9966FF",
-                    ],
-                },
-            ],
-        },
-    });
-});
+//     // Sales chart
+//     const salesCtx = document.getElementById("salesChart").getContext("2d");
+//     new Chart(salesCtx, {
+//         type: "bar",
+//         data: {
+//             labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+//             datasets: [
+//                 {
+//                     label: "Sales",
+//                     data: salesData,
+//                     backgroundColor: "rgba(75, 192, 192, 0.2)",
+//                     borderColor: "rgba(75, 192, 192, 1)",
+//                     borderWidth: 1,
+//                 },
+//             ],
+//         },
+//     });
+
+//     // Category chart
+//     const categoryCtx = document
+//         .getElementById("categoryChart")
+//         .getContext("2d");
+//     new Chart(categoryCtx, {
+//         type: "doughnut",
+//         data: {
+//             labels: ["Electronics", "Clothing", "Books", "Home", "Toys"],
+//             datasets: [
+//                 {
+//                     data: categoryData,
+//                     backgroundColor: [
+//                         "#FF6384",
+//                         "#36A2EB",
+//                         "#FFCE56",
+//                         "#4CAF50",
+//                         "#9966FF",
+//                     ],
+//                 },
+//             ],
+//         },
+//     });
+// });
--- SAP/main.py
+++ SAP/main.py
@@ -9,7 +9,7 @@ import logging
 def main():
     # read the Excel file
 
-    logger = logging.getLogger('cmdstanpy')
+    logger = logging.getLogger("cmdstanpy")
     logger.addHandler(logging.NullHandler())
     logger.propagate = False
     logger.setLevel(logging.CRITICAL)
@@ -35,22 +35,28 @@ def main():
             row_list.append(row)
 
             if data.shape[0] > 1:
-                data.rename(columns={'Date': 'ds', 'Sales': 'y'}, inplace=True)
-                data['ds'] = pd.to_datetime(data['ds'])
+                data.rename(columns={"Date": "ds", "Sales": "y"}, inplace=True)
+                data["ds"] = pd.to_datetime(data["ds"])
 
-                m = Prophet(yearly_seasonality=True, changepoint_range=0.8, changepoint_prior_scale=0.05)
-                m.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+                m = Prophet(
+                    yearly_seasonality=True,
+                    changepoint_range=0.8,
+                    changepoint_prior_scale=0.05,
+                )
+                m.add_seasonality(name="monthly", period=30.5, fourier_order=5)
                 m.fit(data)
-                future = m.make_future_dataframe(periods=24, freq='MS')
+                future = m.make_future_dataframe(periods=24, freq="MS")
 
                 models[id] = m
                 forecasts[id] = m.predict(future)
 
             id = str(row["Product_ID"])
 
-    fig = models.get('15036').plot(forecasts.get('15036'), uncertainty=False, plot_cap=False, include_legend=True)
+    fig = models.get("15036").plot(
+        forecasts.get("15036"), uncertainty=False, plot_cap=False, include_legend=True
+    )
     fig.show()
-    fig = models.get('15036').plot_components(forecasts.get('15036'))
+    fig = models.get("15036").plot_components(forecasts.get("15036"))
     fig.show()
 
 
