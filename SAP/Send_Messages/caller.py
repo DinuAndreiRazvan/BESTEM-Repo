36cd34edb5b154c891f002c15b76913b63da18b9
Dinu Andrei-Razvan <anndrei014@gmail.com>
2023-12-10 06:46:58 +0200

    Done messages

---
+++ SAP/Send_Messages/caller.py
@@ -0,0 +1,5 @@
+from send_message import *
+
+send_email('contacts.txt', 'message.txt')
+send_Wapp_Message('I am Roby the Robot, have a nice Day! *_*', 'contacts.txt')
+send_sms('I am Roby the Robot, have a nice Day! *_*', 'contacts.txt')
--- SAP/Send_Messages/send_message.py
+++ SAP/Send_Messages/send_message.py
@@ -1,7 +1,10 @@
+# Importing the Required Library
 import os
 import ssl
 import smtplib
+import pywhatkit
 from string import Template
+from twilio.rest import Client
 from email.message import EmailMessage
 
 
@@ -9,12 +12,14 @@ from email.message import EmailMessage
 def get_contacts(filename):
     names = []
     emails = []
+    phones = []
     contacts_file = open(filename, mode='r', encoding='utf-8')
     for a_contact in contacts_file:
         names.append(a_contact.split()[0])
         emails.append(a_contact.split()[1])
+        phones.append(a_contact.split()[2])
     contacts_file.close
-    return names, emails
+    return names, emails, phones
 
 
 # Returns a Template object comprising the contents of the 
@@ -25,8 +30,9 @@ def read_template(filename):
     return Template(template_file_content)
 
 # send email function
-def send_email():
+def send_email(File_contacts, File_message):
     sender = 'anndrei014@gmail.com'
+    # EMAIL_PASSWORD should be in bashrc/zshrc..
     app_psswd = os.environ.get('EMAIL_PASSWORD')
     # receiver = 'andreidinu408@yahoo.com'
 
@@ -34,8 +40,8 @@ def send_email():
     # message = 'Hello My Neighbours'
 
     # read contacts
-    names, emails = get_contacts('contacts.txt')
-    message_template = read_template('message.txt')
+    names, emails, phones = get_contacts(File_contacts)
+    message_template = read_template(File_message)
 
     # For each contact, send the email:
     for receiver_name, receiver_email in zip(names, emails):
@@ -54,5 +60,35 @@ def send_email():
             smtp.login(sender, app_psswd)
             smtp.sendmail(sender, receiver_email, em.as_string())
 
-if __name__ == '__main__':
-    send_email()
+# Do not touch anything when running
+def send_Wapp_Message(message, File_contacts):
+    # Defining the Phone Number and Message
+    names, emails, phones = get_contacts(File_contacts)
+
+    # Sending the WhatsApp Message
+    for phone_number in phones:
+        pywhatkit.sendwhatmsg_instantly(phone_number, message, 4, True, 1)
+
+    # Displaying a Success Message
+    print("WhatsApp message sent!")
+
+# Sender and Receiver should have phone numbers verified by Twilio 
+def send_sms(message, File_contacts):
+    # Defining the Phone Number and Message
+    names, emails, phones = get_contacts(File_contacts)
+    # ACCOUNT_SID, AUTH_TOKEN and TWILIO_NUMBER should be defined in bashrc/zshrc..
+    account_sid = os.environ.get('ACCOUNT_SID')
+    auth_token = os.environ.get('AUTH_TOKEN')
+    twilio_number = os.environ.get('TWILIO_NUMBER')
+
+    client = Client(account_sid, auth_token)
+    for p in phones:
+        sms = client.messages.create(
+            body = message,
+            from_ = twilio_number,
+            to = p
+        )
+
+        print(sms.body)
+
+
